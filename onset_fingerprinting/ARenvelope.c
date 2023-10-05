97f5e9c961ba979ba95ab05d9d571444b2742de8
Tim Loderhose <tim@loderhose.com>
2023-10-05 12:09:52 +0900

    Define variables only once in ARenvelope process

--- onset_fingerprinting/ARenvelope.c
+++ onset_fingerprinting/ARenvelope.c
@@ -1,16 +1,19 @@
+#include <math.h>
 #include <stdio.h>
 
 // gcc -shared -o ARenvelope.so -fPIC -Ofast ARenvelope.c
 
-void process(float *x, float *y, float attack, float release, int size, int num_samples) {
-    int index, prev_index;
-    for (int j = 0; j < num_samples; ++j) {
-        for (int i = 0; i < size; ++i) {
+void process(float *x, float *y, float attack, float release, int size,
+             int num_samples) {
+    int i, j, index, prev_index;
+    float xi, yi;
+    for (j = 0; j < num_samples; ++j) {
+        for (i = 0; i < size; ++i) {
             index = j * size + i;
-            prev_index = (j > 0) ? (j - 1) * size + i : (num_samples - 1) * size + i;
-
-            float xi = x[index];
-            float yi = y[prev_index];
+            prev_index =
+                (j > 0) ? (j - 1) * size + i : (num_samples - 1) * size + i;
+            xi = x[index];
+            yi = y[prev_index];
 
             if (xi > yi) {
                 y[index] = yi + attack * (xi - yi);
