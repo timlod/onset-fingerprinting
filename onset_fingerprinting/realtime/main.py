183f1597c746af69d828cf3aca280cfbadd3928a
Tim Loderhose <tim@loderhose.com>
2024-04-08 14:35:16 +0200

    Use realtime.audio instead of Loop

--- onset_fingerprinting/realtime/main.py
+++ onset_fingerprinting/realtime/main.py
@@ -1,19 +1,15 @@
+import threading
+from multiprocessing import Process
+from pathlib import Path
+
 import numpy as np
 import pedalboard
 import rtmidi
 import sounddevice as sd
 import soundfile as sf
-
-import threading
-from multiprocessing import Process
-
-from loopmate import config, recording
-from loopmate.actions import (
-    BackCaptureTrigger,
-    Effect,
-    RecordTrigger,
-)
-from loopmate.loop import Audio, ExtraOutput, Loop
+from loopmate import recording
+from loopmate.actions import BackCaptureTrigger, Effect, RecordTrigger
+from onset_fingerprinting.realtime import audio, config
 
 ## TODOs:
 # record all required channels somehow
@@ -22,24 +18,24 @@ from loopmate.loop import Audio, ExtraOutput, Loop
 # implement some things to control/map
 
 
-def plan_callback(loop: Loop):
+def plan_callback(pr: audio.PlayRec):
     """Callback which picks up triggers/actions from the plan queue.
 
-    :param loop: Loop object containing Actions
+    :param pr: PlayRec object containing Actions
     """
     while True:
         print("plan")
-        trigger = loop.actions.plans.get()
+        trigger = pr.actions.plans.get()
         if isinstance(trigger, RecordTrigger):
             print("Record in plan_callback")
             # TODO: this will run into trouble if result_type == 8
-            if loop.rec.data.result_type == 0:
-                loop.start_recording()
+            if pr.rec.data.result_type == 0:
+                pr.start_recording()
             else:
-                loop.stop_recording()
+                pr.stop_recording()
             continue
         elif isinstance(trigger, BackCaptureTrigger):
-            loop.backcapture(trigger.n_loops)
+            pr.backcapture(trigger.n_loops)
             continue
         elif isinstance(trigger, bool):
             break
@@ -65,6 +61,9 @@ def ondemand_target():
 
 
 if __name__ == "__main__":
+    ml_conf = config.load_setup(
+        Path(__name__).parent.parent.parent / "data" / "demo" / "conf.json"
+    )
     with recording.RecAudio(config.REC_N, config.N_CHANNELS) as rec:
         ap = Process(target=analysis_target)
         ap2 = Process(target=ondemand_target)
@@ -72,18 +71,8 @@ if __name__ == "__main__":
         ap2.start()
 
         print(sd.query_devices())
-        clave, _ = sf.read("../data/clave.wav", dtype=np.float32)
-        clave = np.concatenate(
-            (
-                1 * clave[:, None],
-                np.zeros((config.SR - len(clave), 1), dtype=np.float32),
-            )
-        )
-        loop = Loop(rec, Audio(clave))
-        loop.start()
-
-        if config.HEADPHONE_DEVICE != config.DEVICE:
-            hl = ExtraOutput(loop)
+        pr = audio.PlayRec(rec, ml_conf)
+        pr.start()
 
         # Some example effects that can be applied. TODO: make more
         # interesting/intuitive
@@ -91,11 +80,9 @@ if __name__ == "__main__":
         ds = pedalboard.Distortion(drive_db=20)
         delay = pedalboard.Delay(0.8, 0.1, 0.3)
         limiter = pedalboard.Limiter()
-        loop.actions.append(
-            Effect(0, 10000000, lambda x: limiter(x, config.SR))
-        )
+        pr.actions.append(Effect(0, 10000000, lambda x: limiter(x, config.SR)))
 
-        plan_thread = threading.Thread(target=plan_callback, args=(loop,))
+        plan_thread = threading.Thread(target=plan_callback, args=(pr,))
         plan_thread.start()
         ap.join()
         ap2.join()
