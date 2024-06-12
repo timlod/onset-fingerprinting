59e727f85600c799087267c751e8bf2a052cc64b
Tim Loderhose <tim@loderhose.com>
2024-06-12 23:01:21 +0200

    WIP model stuff

--- onset_fingerprinting/model.py
+++ onset_fingerprinting/model.py
@@ -143,6 +143,7 @@ class RNN(L.LightningModule):
         rnn_type: str = "GRU",  # Options: 'LSTM', 'GRU', 'RNN'
         batch_first: bool = True,
         bidirectional: bool = False,
+        bias: bool = True,
         lr: float = 1e-3,
         activation=nn.SiLU,
     ) -> None:
@@ -179,31 +180,23 @@ class RNN(L.LightningModule):
             dropout=dropout_rate if num_layers > 1 else 0,
             batch_first=batch_first,
             bidirectional=bidirectional,
+            bias=bias,
         )
-
+        self.layer_norm = nn.LayerNorm(hidden_size)
         multiplier = 2 if bidirectional else 1
+        # maybe remove
+        self.attention = nn.MultiheadAttention(
+            hidden_size * multiplier, 2, batch_first=True, dropout=dropout_rate
+        )
         self.fc = nn.Linear(hidden_size * multiplier, output_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.permute(0, 2, 1)
-        h0 = torch.zeros(
-            self.num_layers * (2 if self.bidirectional else 1),
-            x.size(0),
-            self.hidden_size,
-        ).to(x.device)
-        c0 = torch.zeros(
-            self.num_layers * (2 if self.bidirectional else 1),
-            x.size(0),
-            self.hidden_size,
-        ).to(x.device)
-
-        if isinstance(self.rnn, nn.LSTM):
-            out, _ = self.rnn(x, (h0, c0))
-        else:
-            out, _ = self.rnn(x, h0)
-
-        out = out[:, -1, :]  # Take the output of the last time step
-        out = self.fc(out)
+        out, _ = self.rnn(x)
+        out = self.layer_norm(out)
+        # out = out[:, -1, :]  # Take the output of the last time step
+        out, _ = self.attention(out, out, out, need_weights=False)
+        out = self.fc(out.mean(1))
         return out
 
     def training_step(self, batch, batch_idx):
@@ -231,14 +224,149 @@ class RNN(L.LightningModule):
         return loss
 
     def configure_optimizers(self):
+        optimizer = optim.NAdam(
+            self.parameters(), lr=self.lr, weight_decay=1e-4
+        )
+        # scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        #     optimizer, factor=0.5, patience=100
+        # )
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 3000)
+        # scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        #     optimizer, 250, 1
+        # )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+                "frequency": 1,
+            },
+        }
+
+
+class CNNRNN(L.LightningModule):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        channels: int = 3,
+        layer_sizes: list[int] = [8, 16],
+        kernel_size: int = 3,
+        dropout_rate: float = 0.5,
+        n_hidden: int = 64,
+        n_rnn_layers: int = 1,
+        loss=F.l1_loss,
+        batch_norm=False,
+        pool=False,
+        padding=1,
+        dilation=1,
+        groups=1,
+        lr=1e-3,
+        activation=nn.SiLU,
+    ) -> None:
+        """
+        A flexible CNN architecture for audio processing tasks.
+
+        :param window_size: The size of the 1D audio window for each sensor.
+        :param output_size: The dimensionality of the output (e.g., 2D
+            coordinates).
+        :param channels: Number of input channels (sensors).
+        :param conv_layers_config: List of dictionaries defining each
+            convolutional layer configuration.
+        :param dropout_rate: Dropout rate applied after all convolutional
+            layers.
+        """
+        super().__init__()
+        self.conv_layers = nn.Sequential()
+
+        current_channels = channels
+        # Input size to the first layer
+        inp = torch.zeros(1, channels, input_size)
+        for i, layer_size in enumerate(layer_sizes):
+            conv = nn.Conv1d(
+                in_channels=current_channels,
+                out_channels=layer_size,
+                kernel_size=kernel_size,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+            )
+            inp = conv(inp)
+            self.conv_layers.add_module(f"conv{i+1}", conv)
+            self.conv_layers.add_module(f"act{i+1}", activation())
+            if batch_norm:
+                self.conv_layers.add_module(
+                    f"bn{i+1}", nn.BatchNorm1d(layer_size)
+                )
+            if pool:
+                mp = nn.MaxPool1d(kernel_size=2, stride=2)
+                self.conv_layers.add_module(f"pool{i+1}", mp)
+                inp = mp(inp)
+            current_channels = layer_size
+
+        self.dropout = nn.Dropout(dropout_rate)
+        self.rnn = nn.GRU(
+            inp.shape[2],
+            n_hidden,
+            n_rnn_layers,
+            batch_first=True,
+            dropout=dropout_rate,
+        )
+        inp, _ = self.rnn(inp)
+        # self.fc = nn.Linear(rnn_hidden, output_size)
+
+        self.attention = nn.MultiheadAttention(
+            inp.shape[2], 2, batch_first=True, dropout=dropout_rate
+        )
+        self.fc = nn.Linear(inp.shape[2], output_size)
+        inp = self.fc(inp)
+        self.loss = loss
+        self.lr = lr
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_layers(x)
+        # x = torch.flatten(x, start_dim=1)
+        x = self.dropout(x)
+        x, _ = self.rnn(x)
+        x, _ = self.attention(x, x, x, need_weights=False)
+        # Consider using all timesteps
+        # x = self.fc(x[:, -1, :])
+        x = self.fc(x.mean(1))
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        out = self(x)
+        loss = self.loss(out, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        out = self(x)
+        loss = F.l1_loss(out, y)
+        self.log("val_loss", loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        out = self(x)
+        loss = F.l1_loss(out, y)
+        self.log("hp_metric", loss)
+        plots.cartesian_circle(out.cpu().detach().numpy())
+        self.logger.experiment.add_figure("test", plt.gcf())
+        plt.close()
+        return loss
+
+    def configure_optimizers(self):
         optimizer = optim.NAdam(self.parameters(), lr=self.lr)
         # scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         #     optimizer, factor=0.5, patience=100
         # )
-        # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 3000)
-        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer, 250, 1
-        )
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 100)
+        # scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        #     optimizer, 250, 1
+        # )
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
